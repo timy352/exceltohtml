d555c47bf27b7c221de557e1c08b3f87991fb6f5
Timothy Edwards <91966621+timy352@users.noreply.github.com>
2024-11-29 16:50:08 +0000

    Add files via upload
    A sample basic file for testing purposes

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<LINK REL="STYLESHEET" TYPE="text/css" HREF="/word-htm.css?id=<?= time() ?>" media="screen">
+</head>
+
+<body>
+<?php
+require_once('excelphp.php');
+$rt = new ExcelPHP(false);
+$text = $rt->readDocument('sample.xlsx','');
+echo $text;
+// The following two lines are optional if required to save the html text to a file (xlfile.php)
+$myfile = fopen("xlfile.php", "w") or die("Unable to open file!");
+fwrite($myfile, $text);
+?>
+</body>
